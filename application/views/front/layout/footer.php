ccf8e701c6dee6121ae8a6924c0d0bc4edc011f8
adeluthfi07 <adeluthfi0710@gmail.com>
2021-10-30 11:49:08 +0700

    k

--- application/views/front/layout/footer.php
+++ application/views/front/layout/footer.php
@@ -59,9 +59,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
     <script src="<?=base_url()?>assets/js/plugins.js"></script>
     <script src="<?=base_url()?>assets/js/theme.js"></script>
-    <svript>
-    location.reload(true);
-    </svript>
+ 
 </body>
 
 </html>
