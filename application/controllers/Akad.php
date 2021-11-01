2e378df5958047678249b5771b6b6fa7e6855aeb
cadiakmedia <cadiakmedia@gmail.com>
2021-11-01 13:30:13 +0700

    kantor

--- application/controllers/Akad.php
+++ application/controllers/Akad.php
@@ -1,9 +1,10 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Akad extends MY_Controller {
-	
-	public function __construct()
+class Akad extends MY_Controller
+{
+
+    public function __construct()
     {
         parent::__construct();
         date_default_timezone_set('Asia/Jakarta');
@@ -13,87 +14,85 @@ class Akad extends MY_Controller {
         $this->load->library('session');
     }
 
-	public function index()
-	{
+    public function index()
+    {
         $this->load->model('M_crud');
         $slug = $this->uri->segment(2);
-        $tabel ='em_foto';
+        $tabel = 'em_foto';
         $foto = $this->M_crud->get_data_not_del_status($tabel);
         $random = $foto[Rand(0, count($foto) - 1)];
-        $tabel="em_meta";
+        $tabel = "em_meta";
         $meta = $this->M_crud->get_data($tabel);
-        $tabel="user";
+        $tabel = "user";
         $user = $this->M_crud->get_data_user($tabel);
-        $tabel="em_website";
+        $tabel = "em_website";
         $website = $this->M_crud->get_data($tabel);
-        $tabel="em_link";
+        $tabel = "em_link";
         $link = $this->M_crud->get_data_not_del($tabel);
-        $tabel ='em_akad';
-        $nama_slug="slug";
+        $tabel = 'em_akad';
+        $nama_slug = "slug";
 
-        if(!isset($slug)){
-            
-            $akad= $this->M_crud->get_data($tabel);
-            
-            $tabel='em_akad_foto';
+        if (!isset($slug)) {
+
+            $akad = $this->M_crud->get_data($tabel);
+
+            $tabel = 'em_akad_foto';
             $foto_akad = $this->M_crud->get_data($tabel);
-               
-        
+
+
             $meta_array = (array) $meta[0];
             $website_array = (array) $website[0];
-           
-          
+
+
             $data = [
-                'foto'=> $random,
-                'meta_array'=>$meta_array,
-                'user'=>$user,
-                'website_array'=>$website_array,
-                'link'=>$link,
-                'akad'=>$akad,
-                'foto_akad'=>$foto_akad,
-              
-                
+                'foto' => $random,
+                'meta_array' => $meta_array,
+                'user' => $user,
+                'website_array' => $website_array,
+                'link' => $link,
+                'akad' => $akad,
+                'foto_akad' => $foto_akad,
+
+
             ];
-            $this->front($data,'front/konten/menu_akad.php');
+            $this->front($data, 'front/konten/menu_akad.php');
+        } else {
 
-        }else{
+            $akad = $this->M_crud->get_data_by_slug($tabel, $nama_slug, $slug);
+            foreach ($akad as $a) {
+                $id_akad = $a->id;
+            }
+            $pop = "clicked";
+            $pop_akad = $this->M_crud->get_data_pop($tabel, $pop);
 
-        $akad= $this->M_crud->get_data_by_slug($tabel,$nama_slug, $slug);
-        foreach ($akad as $a){
-            $id_akad = $a->id;
+
+
+            $tabel = 'em_akad_foto';
+            $nama_slug = "id_akad";
+
+            $all_foto_akad = $this->M_crud->get_data($tabel);
+            $foto_akad = $this->M_crud->get_data_by_slug($tabel, $nama_slug, $id_akad);
+            $tabel = 'em_akad';
+            $nama_slug = "id";
+
+            $add_clicked = $this->M_crud->edit_data_by_id_click($tabel, $nama_slug, $id_akad);
+
+            $meta_array = (array) $meta[0];
+            $website_array = (array) $website[0];
+            $akad_array = (array) $akad[0];
+            $data = [
+
+                'meta_array' => $meta_array,
+                'user' => $user,
+                'website_array' => $website_array,
+                'link' => $link,
+                'akad_array' => $akad_array,
+                'pop_akad' => $pop_akad,
+                'foto_akad' => $foto_akad,
+                'all_foto_akad' => $all_foto_akad
+
+            ];
+            $this->front($data, 'front/konten/akad.php');
         }
-        $pop = "clicked";
-        $pop_akad= $this->M_crud->get_data_pop($tabel,$pop);
-       
-       
-     
-        $tabel ='em_akad_foto';
-        $nama_slug="id_akad";
-        
-        $all_foto_akad= $this->M_crud->get_data($tabel);
-        $foto_akad= $this->M_crud->get_data_by_slug($tabel,$nama_slug, $id_akad);
-        $tabel ='em_akad';
-        $nama_slug="id";
-      
-        $add_clicked= $this->M_crud->edit_data_by_id_click($tabel,$nama_slug, $id_akad);
-
-        $meta_array = (array) $meta[0];
-        $website_array = (array) $website[0];
-        $akad_array = (array) $akad[0];
-        $data = [
-
-            'meta_array'=>$meta_array,
-            'user'=>$user,
-            'website_array'=>$website_array,
-            'link'=>$link,
-            'akad_array'=>$akad_array,
-            'pop_akad'=>$pop_akad,
-            'foto_akad'=>$foto_akad,
-            'all_foto_akad'=>$all_foto_akad
-            
-        ];
-		$this->front($data,'front/konten/akad.php');
-        }
-	}
+    }
 }
-
